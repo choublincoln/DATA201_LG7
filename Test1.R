c0429a652da7db01e864bb8c7f3dba1a52dad9e9
Pallima <pallimaprakriti@gmail.com>
2026-08-08 14:24:49 +1200

    g

--- Test1.R
+++ Test1.R
@@ -2,3 +2,9 @@ library(tidyverse)
 data1 <- read_csv("data/listings_october.csv")
 
 ggplot(aes(x = longitude, y = price), data = data1) + geom_point() + theme_bw()
+min(data1$price,na.rm=TRUE)
+max(data1$price,na.rm=TRUE)
+sd(data1$price,na.rm=TRUE)
+nrow(data1)
+data1|>
+  count(host_name)

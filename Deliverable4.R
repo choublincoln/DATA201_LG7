c3713b43211c31934af774e59ff76e1b198b39f7
AlexCaesar-dwh <wheble.daniel@gmail.com>
2026-09-14 11:42:43 +1200

    Some Code

--- Deliverable4.R
+++ Deliverable4.R
@@ -10,8 +10,23 @@ listings_oct_to_june <- read_csv("data/listings_oct_to_june.csv") # MUST RUN
 
 #___________________________________________________________________________________________________
 
+drop_column <- function(csvfile, col_name) {"Drops unnecessary columns."
+  drop_file <- csvfile |>
+    select(-all_of(col_name))
+  drop_file
+  }
 
 
+drop_row <- function(csvfile, col_name) {"Drops problematic rows. Not sure if this is necessary."
+  new_data |>
+    filter(!is.na(.csvfile[[col_name]]))
+  new_data
+  }
+
+
+na_rent <- function(csvfile) {"Drops listings that probably have not been rented."
+  
+  }
 #___________________________________________________________________________________________________
 
 

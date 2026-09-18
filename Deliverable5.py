a84a463c686cd5ca413bf0a10718b22b08f68bf5
choublincoln <choublincoln@gmail.com>
2026-09-19 04:20:24 +1200

    Added some intializers

---
+++ Deliverable5.py
@@ -0,0 +1,10 @@
+"Lincoln's code"
+import pandas as pd
+Airbnb_data = pd.read_csv("Data3/Airbnb_listings_cleaned.csv")
+Airbnb_data['sa2_code'] = None
+print(Airbnb_data)
+
+
+#__________________________________________________________________________________________________________________________________________________________
+"Daniel's code"
+

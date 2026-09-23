ed1537926241880e020c423b9b83f10f5f6b4280
Daniel Wheble <wheble.daniel@gmail.com>
2026-09-23 13:41:31 +1200

    Small edited in comment.

--- finding_Chch_median.py
+++ finding_Chch_median.py
@@ -4,4 +4,4 @@ joined_data = pd.read_csv("data3/listings_sa3.csv")
 chch_data = joined_data[joined_data['sa2_code'] == 326600]
 
 chch_median = chch_data['price'].median()
-print(f"Median price for Christchurch is: {chch_median}")
+print(f"Median price for Christchurch Central is: {chch_median}")

195ff09633d22f44ff4127345a1f62749af6568f
Daniel Wheble <wheble.daniel@gmail.com>
2026-09-23 13:40:10 +1200

    Found median price for Christchurch.

---
+++ finding_Chch_median.py
@@ -0,0 +1,7 @@
+import pandas as pd
+joined_data = pd.read_csv("data3/listings_sa3.csv")
+
+chch_data = joined_data[joined_data['sa2_code'] == 326600]
+
+chch_median = chch_data['price'].median()
+print(f"Median price for Christchurch is: {chch_median}")

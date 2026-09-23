a74674962c20cc797c1170990039752b43322405
Pallima <pallimaprakriti@gmail.com>
2026-09-23 13:48:47 +1200

    Add my Code

---
+++ Deliverable5-Pallima.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+df = pd.read_csv("data3/Airbnb_bond_joined_final.csv")
+print(df.columns)
+central = df[df["sa2_code"] == 326600]
+
+central.head()
+median_price = central["price"].median()
+
+print("Median Airbnb price in Christchurch Central:", median_price)
+central_summary = central[
+    [
+        "neighbourhood",
+        "sa2_code",
+        "price"
+    ]
+]
+
+central_summary.head(10)
+central_summary.to_csv(
+    "Christchurch_Central_Airbnb_prices.csv",
+    index=False
+)
